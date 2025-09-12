90479d0001c0d58dc754f4a367dfc092a113d096
Garret Starke <gstarke@live4biking.com>
2025-09-12 16:58:39 -0700

    initial changes

--- account-details-dataconnect/extension/extension.js
+++ account-details-dataconnect/extension/extension.js
@@ -2,10 +2,35 @@
 
 module.exports = {
     name: 'account-details-dataconnect',
-    publisher: '',
+    publisher: 'Garret Starke',
     cards: [{
         type: 'AccountDetailsDataConnectCard',
         source: './src/cards/AccountDetails.jsx',
+        title: 'Account Details - DC',
+        displayCardType: 'Account Details - Data Connect',
+        description: 'Account Details - Data Connect',
+        configuration: {
+            client: [{
+                key: 'payNowUrl',
+                label: 'Pay Now URL',
+                type: 'text',
+                required: false
+            }],
+            server: [{
+                key: 'ethosApiKey',
+                label: 'Ethos API Key',
+                type: 'password',
+                require: true
+            }]
+        },
+        pageRoute: {
+            route: '/',
+            excludeClickSelectors: ['button']
+        }
+    },
+    {
+        type: 'AccountDetailsDataConnectCard',
+        source: './src/cards/MyAccountDetails.jsx',
         title: 'Account Details - DC',
         displayCardType: 'Account Details - Data Connect',
         description: 'Account Details - Data Connect',
